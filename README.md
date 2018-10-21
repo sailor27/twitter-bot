6055d1df2a62f4323ec2452888e680628c34b07b
Sailor Winkelman <sailor.winkelman@modop.com>
2018-10-20 18:50:43 -0700

    Create processing sketch and post to twitter.

---
+++ README.md
@@ -0,0 +1,29 @@
+# **Twitter Bot** 🐬
+
+## Project Description
+
+This little bot can post tweets to twitter using the twit API. Right now, it likes to tweet sketch generated using Processing.
+
+## Built Using:
+
+- Node.js
+- Processing
+- Twit API
+
+## Installation Instructions
+
+in the terminal:
+
+```
+clone this repository
+npm init
+npm install
+```
+
+## Author
+
+**Sailor Winkelman** - [GitHub](https://github.com/sailor27) | [LinkedIn](https://www.linkedin.com/in/sailor-winkelman-62429152)
+
+## Acknowledgements
+
+Project created using a tutorial by Dan Shiffman. Check out his youtube channel, [The Coding Train](https://www.youtube.com/channel/UCvjgXvBlbQiydffZU7m1_aw)
--- bot.js
+++ bot.js
@@ -1,34 +1,52 @@
-const Twit = require("twit");
-const config = require("./config.js");
+const Twit = require("twit"); //twit node package
+const config = require("./config.js"); //twitter account info
 
-const T = new Twit(config);
+const T = new Twit(config); //make twit object to connect to twitter API
 
-// get() --> search tweets 
-const params = {
-    q: 'codingtk;kmrain',
-    count: 2
+const exec = require("child_process").exec; // require child process for triggering Processing script
+const fs = require('fs');
+
+//the twitter bot 🤖🐬
+const doStuff = () => {
+  const cmd = "processing-java --sketch=/Users/sailorwinkelman/Code/twitter-bot/rainbow  --run";
+  
+  const processing = () => {
+      const filename = "rainbow/output.png";
+      const b64content = fs.readFileSync(filename, { encoding: "base64"}); // read file made by processing
+      
+      const uploaded = (err, data, response) => {
+          const id = data.media_id_string;
+          const tweet = {
+              status:
+              "this is a processing sketch posted using node.js #codingtrain ✏️",
+              media_ids: [id]
+            };
+            T.post("statuses/update", tweet, tweeted);
+        };
+        
+        const tweeted = (err, data, response) => {
+            if (err) console.log(`👹 hi ${err}, i'm dad`);
+            if (data) console.log(`Tweet worked, image ${id} uploaded`);
+        };
+        T.post("media/upload", { media_data: b64content }, uploaded); //upload file to twitter
+        
+    };
+    exec(cmd, processing); //run the processing sketch
 }
-
-const gotData = (err, data, response) => {
-    if (err) console.log("👹 hello error");
-    const tweets = data.statuses;
-    tweets.map((tweet, i) => console.log(`tweet ${i}: ${tweet.text}`))
-}
-
-T.get('search/tweets', params, gotData);
-
-// post() --> post tweets
-
-const tweet = {
-    status: "#codingrainbow from node.js 🌈☔️"
-}
-
-const tweeted = (err, data, response) => {
-    if (err) console.log("👹 hello error");
-    if (data) console.log(`Tweet ${data}`)
-};
-
-T.post('statuses/update', tweet, tweeted)
+doStuff(); // run the bot
 
 
-// post() --> post tweets
+
+// get() --> search tweets !!!
+// const params = {
+//     q: 'codingtrain',
+//     count: 2
+// }
+
+// const gotData = (err, data, response) => {
+//     if (err) console.log("👹 hello error");
+//     const tweets = data.statuses;
+//     tweets.map((tweet, i) => console.log(`tweet ${i}: ${tweet.text}`))
+// }
+
+// T.get('search/tweets', params, gotData);
